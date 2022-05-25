a9246cb8be28b7802af64f47123e211f8b08f25e
[Dmaledicte] <[matiasjesusmartel@outlook.es]>
2022-05-25 12:40:35 -0300

    version previa tpo

---
+++ JS/script-gallery.js
@@ -0,0 +1,25 @@
+// Llama al div modal y recopila sus id, selecciona los img con la clase "thumbnail" y efectua el pop-up con el img correcto dependiendo de en cual se clickee
+const modal = document.getElementById('myModal');
+const modalImg = document.getElementById("imgmodal");
+const captionText = document.getElementById("caption");
+var img = document.querySelectorAll('.thumbnail');
+    
+for (var i=0; i<img.length; i++){
+    
+    img[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.getAttribute("data-image");
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+     
+}
+// Efectua la animacion de cerrar el pop-up con la imagen
+modal.onclick = function() {
+    imgmodal.className += " out";
+    setTimeout(function() {
+       modal.style.display = "none";
+       imgmodal.className = "modal-content";
+     }, 400);
+    
+ }       
